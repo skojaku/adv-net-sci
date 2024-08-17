82d84ef6e31d5b74224ad26c7ddfc8c2feb7f7e0
Sadamori Kojaku <skojaku@iu.edu>
2024-08-17 07:57:20 -0400

    Update documentation

--- vis/test.py
+++
@@ -1,9 +0,0 @@
-# %%
-import networkx as nx
-import numpy as np
-G = nx.karate_club_graph()
-A = nx.adjacency_matrix(G)
-labels = np.unique([d[1]['club'] for d in G.nodes(data=True)], return_inverse=True)[1]
-print(labels)
-
-# %%
